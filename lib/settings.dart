35cafdd354d388cdd06c06a42c21550d3586791b
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-09-04 12:14:45 +0100

    Use ToggleButtons for theme selection

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,19 +1,49 @@
 import 'package:daily_diary/main.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
-  void _toggleMode() {
-    MyApp.themeNotifier.value = MyApp.themeNotifier.value == ThemeMode.light
-        ? ThemeMode.dark
-        : ThemeMode.light;
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  List<bool> _selections = [false, true, false];
+
+  void _setTheme(int index) {
+    switch (index) {
+      case 0:
+        MyApp.themeNotifier.value = ThemeMode.light;
+        break;
+      case 1:
+        MyApp.themeNotifier.value = ThemeMode.system;
+        break;
+      case 2:
+        MyApp.themeNotifier.value = ThemeMode.dark;
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: IconButton(
-          onPressed: _toggleMode, icon: Icon(Icons.brightness_medium_outlined)),
-    );
+        appBar: AppBar(title: const Text('Settings')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ToggleButtons(
+            isSelected: _selections,
+            onPressed: (int index) {
+              _selections = [false, false, false];
+              _selections[index] = true;
+              _setTheme(index);
+            },
+            children: const [
+              Text('Light'),
+              Text('System'),
+              Text('Dark'),
+            ],
+          ),
+        ));
   }
 }
