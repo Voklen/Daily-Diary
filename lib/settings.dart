532f004d71b0aa348a3786bbf47c1e12d733f2c3
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-11-27 09:36:58 +0000

    Prettify settings page and change default color

--- lib/settings.dart
+++ lib/settings.dart
@@ -6,7 +6,7 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 class Settings {
   ThemeMode theme = ThemeMode.system;
   double fontSize = 16;
-  Color colorScheme = const Color.fromRGBO(255, 193, 7, 1);
+  Color colorScheme = const Color.fromRGBO(138, 111, 161, 255);
 }
 
 class SettingsNotifier extends ValueNotifier<Settings> {
@@ -45,6 +45,8 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  static const spaceBetweenSettings = 12.0;
+
   final settings = SettingsStorage();
   final _fontSizeController = TextEditingController(
     text: _getFontSize().toString(),
@@ -100,9 +102,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
           children: [
             Text(
               "Theme:",
@@ -118,26 +119,44 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 _setTheme(index);
               },
               renderBorder: false,
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(8),
               children: const [
                 Text('Light'),
                 Text('System'),
                 Text('Dark'),
               ],
             ),
+            const SizedBox(height: spaceBetweenSettings),
             Text(
               "Font size:",
               style: Theme.of(context).textTheme.titleMedium,
             ),
-            TextField(
-              controller: _fontSizeController,
-              onChanged: _setFontSize,
-              keyboardType: TextInputType.number,
+            Row(
+              children: [
+                SizedBox(
+                  width: 48,
+                  child: TextField(
+                    controller: _fontSizeController,
+                    onChanged: _setFontSize,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
             ),
-            ColorPicker(
-              pickerColor: App.settingsNotifier.value.colorScheme,
-              onColorChanged: _setColourScheme,
-            )
+            const SizedBox(height: spaceBetweenSettings),
+            Text(
+              "App color scheme:",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ColorPicker(
+                pickerColor: App.settingsNotifier.value.colorScheme,
+                onColorChanged: _setColourScheme,
+                enableAlpha: false,
+                colorPickerWidth: 250,
+              ),
+            ),
           ],
         ),
       ),
