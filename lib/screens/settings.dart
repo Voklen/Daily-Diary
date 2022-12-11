3443892bb7ff216a7a0ee579677540e4d1d0ddb2
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-12-11 18:43:53 +0000

    Extract settings to widgets and inline labels

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,4 @@
 import 'package:daily_diary/main.dart';
-import 'package:daily_diary/storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
@@ -11,12 +10,28 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  static const spaceBetweenSettings = 12.0;
+  static const padding = EdgeInsets.only(bottom: 12);
 
-  final settings = SettingsStorage();
-  final _fontSizeController = TextEditingController(
-    text: _getFontSize().toString(),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            Padding(padding: padding, child: ThemeSetting()),
+            Padding(padding: padding, child: FontSetting()),
+            const Padding(padding: padding, child: ColorSetting()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThemeSetting extends StatefulWidget {
+  ThemeSetting({super.key});
 
   List<bool> _selections = _getTheme();
 
@@ -31,6 +46,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
     }
   }
 
+  @override
+  State<ThemeSetting> createState() => _ThemeSettingState();
+}
+
+class _ThemeSettingState extends State<ThemeSetting> {
   _setTheme(int index) {
     switch (index) {
       case 0:
@@ -43,89 +63,107 @@ class _SettingsScreenState extends State<SettingsScreen> {
         App.settingsNotifier.setTheme(ThemeMode.dark);
         break;
     }
-    settings.setTheme(App.settingsNotifier.value.theme);
+    App.settings.setTheme(App.settingsNotifier.value.theme);
   }
 
-  static double _getFontSize() => App.settingsNotifier.value.fontSize;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Theme:",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const SizedBox(width: 5),
+        ToggleButtons(
+          isSelected: widget._selections,
+          onPressed: (int index) {
+            widget._selections = [false, false, false];
+            setState(() {
+              widget._selections[index] = true;
+            });
+            _setTheme(index);
+          },
+          renderBorder: false,
+          borderRadius: BorderRadius.circular(8),
+          children: const [
+            Text('Light'),
+            Text('System'),
+            Text('Dark'),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class FontSetting extends StatelessWidget {
+  FontSetting({super.key});
+
+  final _fontSizeController = TextEditingController(
+    text: App.settingsNotifier.value.fontSize.toString(),
+  );
 
   _setFontSize(String fontSizeString) {
     try {
       final fontSize = double.parse(fontSizeString);
       App.settingsNotifier.setFontSize(fontSize);
-      settings.setFontSize(fontSize);
+      App.settings.setFontSize(fontSize);
     } on FormatException {
       return;
     }
   }
 
-  _setColourScheme(Color colorScheme) {
-    settings.setColorScheme(colorScheme);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Font size:",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const SizedBox(width: 5),
+        SizedBox(
+          width: 48,
+          height: 24,
+          child: TextField(
+            controller: _fontSizeController,
+            onChanged: _setFontSize,
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorSetting extends StatelessWidget {
+  const ColorSetting({super.key});
+
+  _setColorScheme(Color colorScheme) {
+    App.settings.setColorScheme(colorScheme);
     App.settingsNotifier.setColorScheme(colorScheme);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            Text(
-              "Theme:",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            ToggleButtons(
-              isSelected: _selections,
-              onPressed: (int index) {
-                _selections = [false, false, false];
-                setState(() {
-                  _selections[index] = true;
-                });
-                _setTheme(index);
-              },
-              renderBorder: false,
-              borderRadius: BorderRadius.circular(8),
-              children: const [
-                Text('Light'),
-                Text('System'),
-                Text('Dark'),
-              ],
-            ),
-            const SizedBox(height: spaceBetweenSettings),
-            Text(
-              "Font size:",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 48,
-                  child: TextField(
-                    controller: _fontSizeController,
-                    onChanged: _setFontSize,
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: spaceBetweenSettings),
-            Text(
-              "App color scheme:",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ColorPicker(
-                pickerColor: App.settingsNotifier.value.colorScheme,
-                onColorChanged: _setColourScheme,
-                enableAlpha: false,
-                colorPickerWidth: 250,
-              ),
-            ),
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "App color scheme:",
+          style: Theme.of(context).textTheme.titleMedium,
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ColorPicker(
+            pickerColor: App.settingsNotifier.value.colorScheme,
+            onColorChanged: _setColorScheme,
+            enableAlpha: false,
+            colorPickerWidth: 250,
+          ),
+        ),
+      ],
     );
   }
 }
