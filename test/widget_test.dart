fd2664a369fcd386256a7e7da3aa9f065b07fcb5
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-12-14 21:57:14 +0000

    Write widget tests
    I've learned my lesson

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:daily_diary/main.dart';
+import 'package:daily_diary/screens/settings.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Appbar icons smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const App());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Daily Diary'), findsOneWidget);
+    expect(find.byIcon(Icons.list_outlined), findsOneWidget);
+    expect(find.byIcon(Icons.settings), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Settings theme buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: SettingsScreen(),
+    ));
+
+    await tester.tap(find.text('Dark'));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('System'));
+    await tester.pump();
+    await tester.tap(find.text('Light'));
+    await tester.pump();
   });
 }
