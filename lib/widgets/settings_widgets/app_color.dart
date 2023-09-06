e2813baa18143737b1fc8ce138da9602549b9a32
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-06 16:00:34 +0100

    Make color setting more visually consistent

--- lib/widgets/settings_widgets/app_color.dart
+++ lib/widgets/settings_widgets/app_color.dart
@@ -5,38 +5,88 @@ import 'package:daily_diary/screens/settings.dart';
 
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-class ColorSetting extends StatelessWidget implements SettingTile {
+class ColorSetting extends StatefulWidget implements SettingTile {
   const ColorSetting({super.key});
 
   @override
   Future<ColorSetting> newDefault() async {
     await App.settingsNotifier.setColorSchemeToDefault();
-    return const ColorSetting();
-  }
-
-  _setColorScheme(Color colorScheme) {
-    App.settingsNotifier.setColorScheme(colorScheme);
+    return ColorSetting(
+      key: UniqueKey(),
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "App color scheme:",
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
+  State<ColorSetting> createState() => _ColorSettingState();
+}
+
+class _ColorSettingState extends State<ColorSetting> {
+  void _setColorScheme(Color colorScheme) async {
+    await App.settingsNotifier.setColorScheme(colorScheme);
+    setState(() {});
+  }
+
+  void _showColorPicker() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Pick a color!'),
+        content: SingleChildScrollView(
           child: ColorPicker(
             pickerColor: App.settingsNotifier.value.colorScheme,
             onColorChanged: _setColorScheme,
             enableAlpha: false,
-            colorPickerWidth: 250,
           ),
         ),
-      ],
+        actions: [
+          ElevatedButton(
+            child: const Text('Got it'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text("App color scheme"),
+      trailing: ColorIndicator(
+        App.settingsNotifier.value.colorScheme,
+        key: UniqueKey(),
+      ),
+      onTap: _showColorPicker,
+    );
+  }
+}
+
+/// Simple round color indicator.
+class ColorIndicator extends StatelessWidget {
+  const ColorIndicator(
+    this.color, {
+    Key? key,
+    this.width = 50.0,
+    this.height = 50.0,
+  }) : super(key: key);
+
+  final Color color;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(1000.0)),
+        border: Border.all(color: const Color(0xffdddddd)),
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(1000.0)),
+        child: CustomPaint(painter: IndicatorPainter(color)),
+      ),
     );
   }
 }
